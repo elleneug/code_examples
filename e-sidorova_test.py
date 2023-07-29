41a371de49b73494ee2e197c5d0c1f5a2a02df36
elleneug <56693605+elleneug@users.noreply.github.com>
2023-07-29 23:06:01 +0300

    Add files via upload

---
+++ e-sidorova_test.py
@@ -0,0 +1,42 @@
+"""
+Тестовый даг
+"""
+from airflow import DAG
+from airflow.utils.dates import days_ago
+import logging
+
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+DEFAULT_ARGS = {
+    'start_date': days_ago(2),
+    'owner': 'e-sidorova',
+    'poke_interval': 600
+}
+
+with DAG("ds_test",
+    schedule_interval='@daily',
+    default_args=DEFAULT_ARGS,
+    max_active_runs=1,
+    tags=['e-sidorova_ds']
+) as dag:
+
+    dummy = DummyOperator(task_id="dummy")
+
+    echo_ds = BashOperator(
+        task_id='echo_ds',
+        bash_command='echo {{ ds }}',
+        dag=dag
+    )
+
+    def hello_world_func():
+        logging.info("Hello World!")
+
+    hello_world = PythonOperator(
+        task_id='hello_world',
+        python_callable=hello_world_func,
+        dag=dag
+    )
+
+    dummy >> [echo_ds, hello_world]
